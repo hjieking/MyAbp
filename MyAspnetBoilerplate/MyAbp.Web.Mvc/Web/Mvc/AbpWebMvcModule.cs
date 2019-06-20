2ad168103f7e29e45f014eed9adfd95280249f7b
Admin <Admin@DESKTOP-N6AFO9P>
2019-06-20 21:54:55 +0800

    提交

---
+++ MyAspnetBoilerplate/MyAbp.Web.Mvc/Web/Mvc/AbpWebMvcModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyAbp.Web.Mvc.Web.Mvc
+{
+    public class AbpWebMvcModule : IHttpModule
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init(HttpApplication application)
+        {
+            application.PostResolveRequestCache += Application_PostResolveRequestCache;
+        }
+
+        private void Application_PostResolveRequestCache(object sender, EventArgs e)
+        {
+            var application = sender as HttpApplication;
+            var context = application.Context;
+
+            context.Response.Write("hello word");
+            context.Response.End();
+        }
+    }
+}
---
+++ MyAspnetBoilerplate/MyAbp.Web/Web/AbpWebApplication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyAbp.Web.Web
+{
+    public abstract class AbpWebApplication:HttpApplication
+    {
+        protected virtual void Application_Start(object sender,EventArgs e)
+        {
+            
+        }
+    }
+}
